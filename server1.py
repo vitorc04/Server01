3b42b04722d53ae602cf4f39eb534c1171a3100f
Vitor Coimbra <47696184+vitorc04@users.noreply.github.com>
2022-02-22 20:29:48 -0300

    Update server1.py

--- server1.py
+++ server1.py
@@ -5,12 +5,13 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
-while True:
-    con, cliente = tcp.accept()
-    print ('Concetado por', cliente)
-    while True:
-        msg = con.recv(1024)
-        if not msg: break
-        print (cliente, msg)
-    print ('Finalizando conexao do cliente', cliente)
-    con.close()
+
+con, cliente = tcp.accept()
+print ('Concetado por', cliente)
+
+msg = con.recv(1024)
+if not msg: break
+print (cliente, msg)
+        
+print ('Finalizando conexao do cliente', cliente)
+con.close()
